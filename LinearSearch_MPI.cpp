7627f0c44e8a40280d33bb998e6f84b83517af71
Tomoiu Cristina <122676393+cristina07a@users.noreply.github.com>
2024-05-06 20:07:11 +0300

    Added content

---
+++ LinearSearch_MPI.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "mpi.h"
+
+using namespace std;
+
+int main() {
+    int rank, size, element;
+    int vector[] = { 1, 2, 4, 7, 9, 12, 14, 17, 17, 17, 17, 30, 32, 35, 38, 40 };
+    int vectorSize = sizeof(vector) / sizeof(vector[0]);
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        cout << "Scrieti termenul pe care il cautati: ";
+        cin >> element;
+
+        for (int processID = 1; processID < size; processID++)
+            MPI_Send(&element, 1, MPI_INT, processID, 1, MPI_COMM_WORLD);
+    }
+    else {
+        MPI_Recv(&element, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    int start = (rank * vectorSize) / size;
+    int end = ((rank + 1) * vectorSize) / size;
+
+    for (int i = start; i < end; i++) {
+        if (i < vectorSize && vector[i] == element) {
+            cout << "Numarul a fost gasit la pozitia " << i << "\n";
+        }
+    }
+
+    MPI_Finalize();
+
+}
---
+++ LinearSearch_Threads.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+using namespace std;
+
+#define SIZE 16
+#define THREAD_MAX 4
+
+int elements[SIZE] = { 1, 2, 4, 7, 9, 12, 14, 17, 11, 17, 17, 17, 32, 35, 38, 40 };
+int found = 0;
+
+void ThreadSearch(int start, int end,int key) {
+    for (int i = start; i < end; i++) {
+        if (elements[i] == key)
+        {
+            found = 1;
+            cout << "S-a gasit elementul cautat la pozitia " << i <<endl;
+        }
+    }
+}
+
+int main() {
+    int key;
+    vector<thread> threads;
+    cout << "Elementul cautat: ";
+    cin >> key;
+    for (int i = 0; i < THREAD_MAX; i++) {
+        threads.push_back(thread(ThreadSearch, i * (SIZE / 4), (i + 1) * (SIZE / 4),key));
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    if (found == 0)
+        cout << "Nu s-a gasit elementul cautat" << endl;
+
+    return 0;
+}
