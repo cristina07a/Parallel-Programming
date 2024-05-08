1625f9e7e8ad6d80a37e59a86cf9327f2714b916
Tomoiu Cristina <122676393+cristina07a@users.noreply.github.com>
2024-05-08 12:21:47 +0300

    Add files via upload

---
+++ LinearSearch_STL.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int element;
+    vector<int> vectorr = { 1, 2, 4, 7, 9, 12, 14, 17, 17, 17, 17, 30, 32, 35, 38, 40 };
+
+    cout << "Scrieti termenul pe care il cautati: ";
+    cin >> element;
+
+    auto it = vectorr.begin();
+    while ((it = find(it, vectorr.end(), element)) != vectorr.end()) {
+        cout << "Numarul a fost gasit la pozitia: " << (it - vectorr.begin()) << endl;
+        ++it;
+    }
+
+    if (it == vectorr.begin()) {
+        cout << "Nu s-a gasit elementul cautat." << endl;
+    }
+
+    return 0;
+}
