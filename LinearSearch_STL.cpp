0ceab3f4d3e6083797583c52fb89050f4cfabe85
Tomoiu Cristina <122676393+cristina07a@users.noreply.github.com>
2024-05-18 17:49:55 +0300

    Update LinearSearch_STL.cpp

--- LinearSearch_STL.cpp
+++ LinearSearch_STL.cpp
@@ -1,22 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
-
+#include <execution>
 using namespace std;
 
 int main() {
-    int element;
+    int element, ok = 0;
     vector<int> vectorr = { 1, 2, 4, 7, 9, 12, 14, 17, 17, 17, 17, 30, 32, 35, 38, 40 };
 
     cout << "Scrieti termenul pe care il cautati: ";
     cin >> element;
 
     auto it = vectorr.begin();
-    while ((it = find(it, vectorr.end(), element)) != vectorr.end()) {
+    while ((it = find(execution::par, it, vectorr.end(), element)) != vectorr.end()) 
+    {
         cout << "Numarul a fost gasit la pozitia: " << (it - vectorr.begin()) << endl;
+        ok = 1;
         ++it;
     }
 
-    if (it == vectorr.begin()) {
+    if (ok == 0) {
         cout << "Nu s-a gasit elementul cautat." << endl;
     }
 
